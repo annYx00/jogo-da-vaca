a25ffdcbe3538ff3f09dfc6ced603d74b8f2dcae
annYx00 <126181287+annYx00@users.noreply.github.com>
2024-05-13 14:20:09 -0300

    Add files via upload

---
+++ imagens.js
@@ -0,0 +1,15 @@
+let Fundo;
+let imagemCarro;
+let imagemCarro1;
+let imagemCarro2;
+let imagemAtor;
+
+
+ function preload(){
+  Fundo = loadImage("imagens/estrada.png");
+  imagemCarro = loadImage("imagens/mercedes.jpg");
+   imagemCarro1 = loadImage("imagens/preto.jpg");
+   imagemCarro2 = loadImage("imagens/magenta.png");
+   imagemAtor = loadImage("imagens/ator-1.png");
+  imagemCarros = [imagemCarro, imagemCarro1, imagemCarro2,imagemCarro, imagemCarro1, imagemCarro2];   
+  }
